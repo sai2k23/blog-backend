6f4f70a2de100cab85b1488e4a569ac654f63acb
KOLLETI SAI KRISHNA <saikrishnakolleti@gmail.com>
2025-06-02 15:06:16 +0530

    Second commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://noteharborplatform.netlify.app", credentials: true }));
 app.use(express.json());
 
 app.use(
